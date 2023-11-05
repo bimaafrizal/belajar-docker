c53331cf8aa8aa405cafa6bf15f9887c5f43896a
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2023-11-05 22:27:41 +0700

    backup restore volume + dasar network

--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -1,6 +1,7 @@
 D:\Golang Project\belajar-docker
+D:\laragon\www\belajar-docker 
 
-docker container create --name nginxbackup --mount "type=bind,source=/D:/Golang Project/belajar-docker/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest
+docker container create --name nginxbackup --mount "type=bind,source=D:\laragon\www\belajar-docker/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest
 
 docker container start nginxbackup
 
@@ -17,4 +18,4 @@ docker container start mongovolume
 # menjalankan container secara langsung
 # gunakan image yang berjalan sekali
 
-docker container run --rm --name ubuntuLatest --mount "type=bind,source=/D:/Golang Project/belajar-docker/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest tar -cvf /backup/mongodata-lagi.tar.gz /data
+docker container run --rm --name ubuntuLatest --mount "type=bind,source=/D:/laragon/www/belajar-docker/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest tar -cvf /backup/mongodata-lagi.tar.gz /data
